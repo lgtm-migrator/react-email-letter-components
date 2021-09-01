92cc9cb90df9a6cb05b9593f4a2c727a87ed15d1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-01 13:30:59 +0300

    adding more components and making it less bugy

--- in-progress/ecommerce/src/Layouts/HeaderLayout/HeaderLayout.jsx
+++ in-progress/ecommerce/src/Layouts/HeaderLayout/HeaderLayout.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+const xxx = ({}) => (
+  <> </>
+);
 
+export default xxx;
 <table border="0" cellpadding="0" cellspacing="0" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;" width="100%">
                     <tbody>
                       <tr>
---
+++ in-progress/ecommerce/src/Layouts/HeroLayout/Separator.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const HeroSeparator = ({}) => (
+  <> </>
+);
+
+export default HeroSeparator;
+
+<tr>
+<td style="vertical-align: top; height: 12px; font-size: 12px; line-height: 12px;" valign="top">
+	&nbsp;
+</td>
+</tr>
+
+
+<tr>
+<td style="vertical-align: top; height: 70px; line-height: 70px; font-size: 70px;" valign="top">
+	&nbsp;
+</td>
+</tr>
+
+
+<tr>
+	<td style="vertical-align: top; height: 63px; line-height: 63px; font-size: 63px;" valign="top">
+	&nbsp;
+	</td>
+</tr>
+
+
+<tr>
+<td style="vertical-align: top; height: 25px; line-height: 25px; font-size: 25px;" valign="top">&nbsp;</td>
+</tr>          
