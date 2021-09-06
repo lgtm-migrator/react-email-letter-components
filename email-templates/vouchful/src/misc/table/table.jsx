f130dbd512680d848c91ece743352368fb10d877
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-06 20:33:18 +0300

    re-organization

---
+++ email-templates/vouchful/src/misc/table/table.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BasicTable } from 'react-emails-components-miscellaneous';
+
+
+// TODO: add styles as props from the outside
+const BasicTable1 = ({children}) => (
+
+  <BasicTable>
+
+    {children}
+
+  </BasicTable>
+
+);
+
+export default BasicTable1;
