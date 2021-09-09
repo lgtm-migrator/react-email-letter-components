1ddf9adca5143e7958828420985cafb94b8e46ee
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-09 23:14:46 +0000

    update

--- email-templates/--empty/ecommerce/src/Typography/Heading/Heading.jsx
+++ email-templates/--empty/ecommerce/src/Typography/Heading/Heading.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const xxx = ({}) => (
+const Heading = ({}) => (
   <> </>
 );
 
-export default xxx;
+export default Heading;
--- email-templates/--empty/ecommerce/src/Typography/HeadingThree/Heading3.jsx
+++ email-templates/--empty/ecommerce/src/Typography/HeadingThree/Heading3.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const xxx = ({}) => (
+const HeadingThree = ({}) => (
   <> </>
 );
 
-export default xxx;
+export default HeadingThree;
