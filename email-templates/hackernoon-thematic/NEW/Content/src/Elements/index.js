dfe511bbb0b1a005a90256cde6fb3ca462b89167
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-17 14:43:28 +0000

    removing body component from hn

---
+++ email-templates/hackernoon-thematic/NEW/Content/src/Elements/index.js
@@ -0,0 +1,18 @@
+/* eslint import/extensions: 0 */
+
+
+
+import CtaList from './ctaList/CtaList.jsx';
+
+import GetPublished from './getpublished/GetPublished.jsx';
+
+import JoinUs from './joinus/JoinUs.jsx';
+
+
+
+export {
+    CtaList,   
+    GetPublished, 
+    JoinUs, 
+     
+};
--- email-templates/hackernoon-thematic/NEW/src/Elements/empty-section/EmptySection.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { Table } from 'react-email-components-table';
-const EmptySection = ({ children }) => (
-  <div>
-    {children}
-  </div>
-);
-
-export default EmptySection;
