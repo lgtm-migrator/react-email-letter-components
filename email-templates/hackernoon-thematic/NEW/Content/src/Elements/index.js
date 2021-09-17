b7bffbb220ed181f0a853cead6e0fa729094d1db
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-17 14:56:40 +0000

    re-organization

--- email-templates/hackernoon-thematic/NEW/Content/src/Elements/index.js
+++ email-templates/hackernoon-thematic/NEW/Content/src/Elements/index.js
@@ -9,6 +9,14 @@ import GetPublished from './getpublished/GetPublished.jsx';
 import JoinUs from './joinus/JoinUs.jsx';
 
 
+import Address from '../../Content/src/Elements/address/Address.jsx';
+import Copyright from './copyrights/Copyrights.jsx';
+
+import NewsletterSponsorshipLink from './newsletter-sponsorship-link/Newsletter.jsx';
+import Unsubscribe from './unsubscribe/Unsubscribe.jsx';
+
+// Address, Copyright, headStyles, 
+//   NewsletterSponsorshipLink, Unsubscribe
 
 export {
     CtaList,   
