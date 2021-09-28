0c7df8ba64835a90ac840409bf2956d60fa986d5
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2021-09-28 19:21:30 +0000

    updating few more templates in order to get everything even worse

--- email-templates/--empty/billing/amazon/Sections/Footer/Footer.jsx
+++ email-templates/--empty/billing/amazon/Sections/Footer/Footer.jsx
@@ -1,1 +1,13 @@
-jsx
+import React from 'react';
+
+import { 
+  ImageLink
+} from 'react-emails-components-miscellaneous';
+
+
+// import { Table, BasicTable } from 'react-email-components-table';
+
+
+const Footer = ({}) => (
+
+);
