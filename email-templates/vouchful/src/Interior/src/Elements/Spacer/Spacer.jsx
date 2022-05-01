e222ce0806bd65351e3bccba71372239af317c51
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-05-01 01:03:29 +0000

    clean up

--- email-templates/vouchful/src/Interior/src/Elements/Spacer/Spacer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Spacer = ({}) => (
-    <tr>
-      <td class="spacer-sm"></td>
-    </tr>       
-);
-
-export default Spacer;
--- email-templates/vouchful/src/Template/TTTemplate.jsx
+++
@@ -1,7 +0,0 @@
-import {
-    Head, 
-} from "react-email-components-template";
-
-// import headCSS from './head-styles/headCSS';
-import HeadStyles from ''
-
